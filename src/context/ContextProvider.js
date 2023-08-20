54d4d123233d5b366871a1002a9996850388fe0f
Dwi Bagaskara <dwibagaskara55@gmail.com>
2023-08-20 15:20:51 +0700

    Navbar

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -15,6 +15,7 @@ export const ContextProvider = (({children}) => {
     return(
         <StateContext.Provider value={{
             activeMenu: activeMenu,
+            setActiveMenu
             }}>
                 {children}
         </StateContext.Provider>
