2c8fb1cf1cdb77daefdf433a7c30339d4dcf24be
Dwi Bagaskara <dwibagaskara55@gmail.com>
2023-08-26 17:40:22 +0700

    Customers

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -11,7 +11,6 @@ export default function Employees() {
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category="Page" title="Employees" />
       <GridComponent
-        id="gridcomp"
         dataSource={employeesData}
         allowPaging
         allowSorting
